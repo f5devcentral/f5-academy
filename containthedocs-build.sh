7094d65930beaf7c1ceb934916435a611f711299
Joshua Murray <murray.joshua@gmail.com>
2024-03-29 12:57:16 -0400

    Initial commit

---
+++ containthedocs-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+
+COMMAND=(/bin/bash -c "sudo pip install -r requirements.txt -U ; make -C docs clean html")
+ARCH=`uname -m`
+
+. ./containthedocs-image-$ARCH
+
+exec docker run --rm -t \
+  -v "$PWD":"$PWD" --workdir "$PWD" \
+  ${DOCKER_RUN_ARGS} \
+  -e "LOCAL_USER_ID=$(id -u)" \
+  ${DOC_IMG} "${COMMAND[@]}"
